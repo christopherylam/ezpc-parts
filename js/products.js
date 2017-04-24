1955a0107725633b93c6bbadcbd526a5283b5a23
itzthanh <thanhhd@uci.edu>
2017-04-24 14:48:58 -0700

    Fixed small table typo and started individual product page

--- js/products.js
+++ js/products.js
@@ -1,8 +1,7 @@
 //products.js
 $(window).ready(function(){
 	$("#products-table img").mouseenter(function(){
-		//$(this).css("width", "10em");
-		$(this).animate({width: "10em"}, "fast").mouseleave(function(){
+		$(this).stop().animate({width: "10em"}, "fast").mouseleave(function(){
 			$(this).animate({width: "8em"}, "fast");
 		});
 	})
